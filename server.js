4d66dc7306ac4293c7e6fd8bcf1dc4f9fe49a0fd
Gulmira Kamalova <gulmira_cullen@mail.ru>
2016-11-22 11:10:54 +0600

    final lection project

--- server.js
+++ server.js
@@ -1,20 +1,64 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
 
+app.use(bodyParser.json());
 
-var middleware = require('./middleware.js');
-
-
-app.use(middleware.logger);
-
-
-app.get('/about', middleware.requireAuthentication, function(req, res){
-	res.send('About us!');
+app.get('/', function(req, res){
+	res.send('ToDo API Root');
 });
 
-app.use(express.static(__dirname + '/public'));
+// GET /todos
+app.get('/todos', function(req, res){
+	res.json(todos);
+});
+
+// GET /todos/:id
+app.get('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(matchedTodo){
+		res.json(matchedTodo);
+	}else{
+		res.status(404).send();
+	}
+});
+
+// POST /todos
+
+app.post('/todos', function(req, res){
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
+	body.id = todoNextId++;
+
+	todos.push(body);
+
+	res.json(body);
+});
+
+app.delete('/todos/:id', function(req,res){
+	var todoId = parseInt(req.params.id,10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(!matchedTodo){
+		res.status(404).json({"error": "no todo found with that id"});
+	}else{
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+});
 
 app.listen(PORT, function(){
-	console.log('Express server started on port ' + PORT + '!');
+	console.log('Express listening on port ' + PORT + '!');
 });
